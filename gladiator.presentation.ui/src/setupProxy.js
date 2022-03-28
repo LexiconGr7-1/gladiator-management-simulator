9626ba80485b32def33d52a6917518e442b26dd8
maqsoodulhasan <alhasanmps@yahoo.com>
2022-03-28 11:05:16 +0200

    fix-ui-proxy-content

--- gladiator.presentation.ui/src/setupProxy.js
+++ gladiator.presentation.ui/src/setupProxy.js
@@ -1,6 +1,6 @@
 ﻿const { createProxyMiddleware } = require("http-proxy-middleware");
 
-const context = ["/weatherforecast"];
+const context = ["/"];
 
 module.exports = function (app) {
   const appProxy1 = createProxyMiddleware(context, {
