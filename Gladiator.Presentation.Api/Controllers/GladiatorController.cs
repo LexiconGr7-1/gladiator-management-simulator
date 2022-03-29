f694797d3eb55328b4d8724f5cc6e12bfa3b88b6
GAMO <gabriel.molander@gmail.com>
2022-03-29 14:18:22 +0200

    Remake and refactor

--- Gladiator.Presentation.Api/Controllers/GladiatorController.cs
+++ Gladiator.Presentation.Api/Controllers/GladiatorController.cs
@@ -4,41 +4,63 @@ using System;
 namespace Gladiator.Presentation.Api.Controllers
 {
     [ApiController]
-    [Route("api/Gladiator")]
+    [Route("api/[controller]")]
     public class GladiatorController : ControllerBase
     {
-        [HttpGet]
-        public ActionResult<String> ReadGladiator(int? id)
-        {  Double _id;
-           if (id != null)
-            {   _id=Convert.ToDouble(id);
-                string ReadGladiator = "ReadGladiator"+Convert.ToString(_id);
-                return ReadGladiator;
-            }
-            else
-            {
-                string ReadGladiator = "ReadGladiator";
-                return ReadGladiator;
-            }
-        }
-        [HttpPost]
-        public ActionResult<String> CreateGladiator()
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetGladiator(int id)
         {
-            string NewGladiator = "NewGladiator";
-            return NewGladiator;
-        }
-        [HttpPut]
-        public ActionResult<String> UpdateGladiator()
-        {
-            string UpdateGladiator = "UpdateGladiator";
-            return UpdateGladiator;
-        }
-        [HttpDelete]
-        public ActionResult<String> DeleteGladiator()
-        {
-            string DeleteGladiator = "DeleteGladiator";
+            if (id < 1)
+                return BadRequest();
 
-            return DeleteGladiator;
+            string gladiator = "Gladiator: " + id;
+
+            return Ok(gladiator);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetGladiators()
+        {
+            string[] gladiators = new string[]
+            {
+                "Gladiator1",
+                "Gladiator2",
+                "Gladiator3",
+                "Gladiator4"
+            };
+
+            return Ok(gladiators);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateGladiator(object gladiator)
+        {
+            if (gladiator == null)
+                return BadRequest();
+
+            string NewGladiator = "CreateGladiator: " + gladiator.ToString();
+
+            return Ok(NewGladiator);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateGladiator(int id, object gladiator)
+        {
+            if (gladiator == null || id < 1)
+                return BadRequest();
+
+            string UpdateGladiator = "UpdateGladiator: " + id + " to " + gladiator.ToString();
+
+            return Ok(UpdateGladiator);
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGladiator(int id)
+        {
+            if (id < 1)
+                return BadRequest();
+
+            string DeleteGladiator = "DeleteGladiator: " + id;
+
+            return Ok(DeleteGladiator);
         }
     }
 }
