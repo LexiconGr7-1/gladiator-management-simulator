6b0716121d78489f5a581ea2f2e7169860a65354
JaniTuoriniemi <janituoriniemi@hotmail.com>
2022-03-29 10:53:03 +0200

    Gladiator Controller returning strings added

---
+++ Gladiator.Presentation.Api/Controllers/GladiatorController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+    namespace Gladiator.Presentation.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[Gladiator]")]
+    public class GladiatorController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<String> CreateGladiator()
+        {
+            string NewGladiator="NewGladiator";
+            return NewGladiator;
+        }
+        [HttpPost]
+        public ActionResult<String> ReadGladiator()
+        {
+            string ReadGladiator = "ReadGladiator";
+            return ReadGladiator;
+        }
+        [HttpPut]
+        public ActionResult<String> UpdateGladiator()
+        {
+            string UpdateGladiator = "UpdateGladiator";
+            return UpdateGladiator;
+        }
+        [HttpDelete]
+        public ActionResult<String> DeleterGladiator()
+        {
+            string DeleteGladiator = "DeleteGladiator";
+
+            return DeleteGladiator;
+        }
+    }
+}
