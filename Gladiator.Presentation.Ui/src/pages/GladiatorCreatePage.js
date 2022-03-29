b95d515933bb725d846039e36c66e47a3236be66
Igor Pieropan <igorpieropan@gmail.com>
2022-03-29 15:39:50 +0200

    create gladiator create page

---
+++ Gladiator.Presentation.Ui/src/pages/GladiatorCreatePage.js
@@ -0,0 +1,19 @@
+const GladiatorCreatePage = () => {
+    return (
+            
+            <div>
+            <h2>Create gladiator [name]</h2>
+            <form>
+                <label htmlFor="name" className="form-label">Name</label>
+                <input type="text" name="name" className="form-control mb-3"/>
+                <label htmlFor="health" className="form-label">Health</label>
+                <input type="number" name="Health" className="form-control mb-3"/>
+                <label htmlFor="strength" className="form-label">Strength</label>
+                <input type="number" name="strength" className="form-control mb-3"/>
+                <button type="submit" className="btn btn-primary">Create</button>
+            </form>
+        </div>
+        )
+}
+
+export default GladiatorCreatePage;
--- Gladiator.Presentation.Ui/src/routes/Views.js
+++ Gladiator.Presentation.Ui/src/routes/Views.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import LandingPage from "../pages/LandingPage";
 import NotFound from "../pages/NotFound";
+import GladiatorCreatePage from "../pages/GladiatorCreatePage";
 
 const Views = () => {
   return (
     <Routes>
       <Route exact path="/" element={<LandingPage />} />
       <Route index element={<LandingPage />} />
+      <Route exact path="/gladiator-create" element={<GladiatorCreatePage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
