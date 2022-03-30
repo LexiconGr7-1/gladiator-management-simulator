e7e741197145b13464dc4999b5f3be5dba88f9d7
Tero Viitala <tero.viitala@outlook.com>
2022-03-30 14:49:42 +0200

    Temp commit

--- gladiator.presentation.ui/src/pages/GladiatorListPage.js
+++ gladiator.presentation.ui/src/pages/GladiatorListPage.js
@@ -1,12 +1,35 @@
+import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 const GladiatorListPage = () => {
+    const [gladiators, setGladiators] = useState([]);
+
+    useEffect(() => {
+        //fetch('https://api.github.com/users/treox')
+        //    .then(res => {
+        //        return res.json();
+        //    })
+        //    .then(data => {
+        //        console.log(data);
+        //        setGladiators(data);
+        //    })
+        //    .catch(err => {
+        //        console.log(err);
+        //    });
+
+            setGladiators([{ id:1, name: "Gladiator1" }, { id:2, name: "Gladiator2" }, { id:3, name: "Gladiator3" }, { id:4, name: "Gladiator4" }]);
+    }, []);
+
+    console.log(gladiators.map((g) => {return g.name}));
+
     return (
         <div>
             <h2>Gladiator List</h2>
             <div>
-                <span className="col">Gladiator name </span>
+                {(gladiators.map((gladiator) => (
+                <div key={gladiator.id}>
+                    <span className="col">{gladiator.name}</span>
                 <Link
-                    to="/gladiator-details"
+                    to="/gladiator-details/" + {gladiator.id}
                     className="btn btn-secondary m-3 col"
                 >
                     Details
@@ -23,7 +46,9 @@ const GladiatorListPage = () => {
                     className="btn btn-secondary m-3 col"
                 >
                     Delete
-                </Link>
+                        </Link>
+                </div>
+                )))}
             </div>
         </div>
     );
