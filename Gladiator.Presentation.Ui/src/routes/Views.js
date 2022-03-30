a3f821851bc80068c8add04a1e5b2935c72cd453
maqsoodulhasan <alhasanmps@yahoo.com>
2022-03-30 15:33:40 +0200

    create gladiator details page

--- Gladiator.Presentation.Ui/src/routes/Views.js
+++ Gladiator.Presentation.Ui/src/routes/Views.js
@@ -1,10 +1,10 @@
 import { Route, Routes } from "react-router-dom";
-
 import LandingPage from "../pages/LandingPage";
 import GladiatorListPage from "../pages/GladiatorListPage";
 import NotFound from "../pages/NotFound";
 import GladiatorCreatePage from "../pages/GladiatorCreatePage";
 import GladiatorEditPage from "../pages/GladiatorEditPage";
+import GladiatorDetailsPage from "../pages/GladiatorDetailsPage";
 
 const Views = () => {
   return (
@@ -13,6 +13,7 @@ const Views = () => {
       <Route exact path="/gladiator-edit" element={<GladiatorEditPage />} />
       <Route exact path="/gladiator-create" element={<GladiatorCreatePage />} />
       <Route exact path="/gladiator-list" element={<GladiatorListPage />} />
+      <Route exact path="/gladiator-details" element={<GladiatorDetailsPage />} />
       <Route index element={<LandingPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
