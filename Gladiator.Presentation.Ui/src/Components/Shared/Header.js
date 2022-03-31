85e3299e694e2793d7fdf1bb0144ee58099f8483
ipieropan <igorpieropan@gmail.com>
2022-03-31 10:37:48 +0200

    changes

--- Gladiator.Presentation.Ui/src/Components/Shared/Header.js
+++ Gladiator.Presentation.Ui/src/Components/Shared/Header.js
@@ -1,9 +1,8 @@
 import React from "react";
-import logo from "../images/350x65.jpg";
 const Header = () => {
     return (
         <header>
-            <img src={logo} alt="Test Image" />
+            Some header content here
         </header>
     );
 };
