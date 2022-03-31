7c42d2d4f7fcfec3fc5d7ef036153a99fd442116
ipieropan <igorpieropan@gmail.com>
2022-03-31 10:44:11 +0200

    create layout

--- gladiator.presentation.ui/src/App.js
+++ gladiator.presentation.ui/src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
+import Layout from "./Components/Shared/Layout";
 import Views from "./routes/Views";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Views />
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Views />
+            </Layout>
+        </BrowserRouter>
+    );
 };
 
 export default App;
