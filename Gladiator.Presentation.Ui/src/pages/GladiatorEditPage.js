4d34a7764b65f0399af488684c764f4514da36dc
Tero Viitala <tero.viitala@outlook.com>
2022-04-05 14:02:09 +0200

    Create player pages

--- Gladiator.Presentation.Ui/src/pages/GladiatorEditPage.js
+++ Gladiator.Presentation.Ui/src/pages/GladiatorEditPage.js
@@ -14,11 +14,11 @@ const GladiatorEditPage = () => {
             <h2>Edit {gladiator.name}</h2>
             <form>
                 <label htmlFor="name" className="form-label">Name</label>
-                <input type="text" name="name" value={gladiator.name} className="form-control mb-3"/>
+                <input type="text" name="name" defaultValue={gladiator.name} className="form-control mb-3"/>
                 <label htmlFor="health" className="form-label">Health</label>
-                <input type="number" name="Health" value={gladiator.health} className="form-control mb-3"/>
+                <input type="number" name="Health" defaultValue={gladiator.health} className="form-control mb-3"/>
                 <label htmlFor="strength" className="form-label">Strength</label>
-                <input type="number" name="strength" value={gladiator.strength} className="form-control mb-3"/>
+                <input type="number" name="strength" defaultValue={gladiator.strength} className="form-control mb-3"/>
                 <button type="submit" className="btn btn-primary">Update</button>
             </form>
         </div>
--- Gladiator.Presentation.Ui/src/routes/Views.js
+++ Gladiator.Presentation.Ui/src/routes/Views.js
@@ -1,19 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
-import GladiatorListPage from "../pages/GladiatorListPage";
 import NotFound from "../pages/NotFound";
+import PlayerListPage from "../pages/PlayerListPage";
+import PlayerDetailsPage from "../pages/PlayerDetailsPage";
+import PlayerCreatePage from "../pages/PlayerCreatePage";
+import PlayerEditPage from "../pages/PlayerEditPage";
+import GladiatorListPage from "../pages/GladiatorListPage";
+import GladiatorDetailsPage from "../pages/GladiatorDetailsPage";
 import GladiatorCreatePage from "../pages/GladiatorCreatePage";
 import GladiatorEditPage from "../pages/GladiatorEditPage";
-import GladiatorDetailsPage from "../pages/GladiatorDetailsPage";
 
 const Views = () => {
   return (
     <Routes>
       <Route exact path="/" element={<LandingPage />} />
-      <Route exact path="/gladiator/edit/:id" element={<GladiatorEditPage />} />
-      <Route exact path="/gladiator/create" element={<GladiatorCreatePage />} />
+      <Route exact path="/player" element={<PlayerListPage />} />
+      <Route exact path="/player/:id" element={<PlayerDetailsPage />} />
+      <Route exact path="/player/create" element={<PlayerCreatePage />} />
+      <Route exact path="/player/edit/:id" element={<PlayerEditPage />} />
       <Route exact path="/gladiator" element={<GladiatorListPage />} />
       <Route exact path="/gladiator/:id" element={<GladiatorDetailsPage />} />
+      <Route exact path="/gladiator/create" element={<GladiatorCreatePage />} />
+      <Route exact path="/gladiator/edit/:id" element={<GladiatorEditPage />} />
       <Route index element={<LandingPage />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
