13a560e49642fabe6a2cac83b2d94adc8feee7b6
ipieropan <igorpieropan@gmail.com>
2022-04-06 09:19:07 +0200

    create layout

--- Gladiator.Presentation.Ui/src/Components/Layout/Header.js
+++ Gladiator.Presentation.Ui/src/Components/Layout/Header.js
@@ -4,7 +4,7 @@ import React from "react";
 const Header = () => {
     return (
         <header>
-            <div>
+            <div className="h-100 row align-items-center">
                 <Link to="/gladiator-create">Create Gladiator</Link>
                 <Link to="/gladiator-edit">Edit Gladiator</Link>
                 <Link to="/gladiator-list">Gladiator list</Link>
