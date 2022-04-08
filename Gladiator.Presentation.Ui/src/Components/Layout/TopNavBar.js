0b85aa7cd7553b3d2450c82a7c5203d17349cb2e
ipieropan <igorpieropan@gmail.com>
2022-04-08 11:30:28 +0200

    add logo to navbar

--- Gladiator.Presentation.Ui/src/Components/Layout/TopNavBar.js
+++ Gladiator.Presentation.Ui/src/Components/Layout/TopNavBar.js
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
 
 const TopNavBar = () => {
+    const LogoStyle = {
+        "height": "100px",
+        "width": "89px"
+    }
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Gladiators</Link>
+                <Link to="/"> <img src="images/logogladiator.png" alt="Gladiator Logo" style={LogoStyle} /></Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
