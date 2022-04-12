377d4c6201aaf9670dbf18442d1ee30b0b6c0d1b
Tero Viitala <tero.viitala@outlook.com>
2022-04-12 11:20:57 +0200

    Fix tests

--- Gladiator.Presentation.Ui/src/pages/GladiatorEditPage.test.js
+++ Gladiator.Presentation.Ui/src/pages/GladiatorEditPage.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import GladiatorEditPage from "./GladiatorEditPage";
 
 test("renders gladiator edit page", () => {
-    render(<GladiatorEditPage />);
+    render(<GladiatorEditPage />, { wrapper: BrowserRouter });
     const linkElement = screen.getByText(/Update|Loading.../i);
     expect(linkElement).toBeInTheDocument();
 });
