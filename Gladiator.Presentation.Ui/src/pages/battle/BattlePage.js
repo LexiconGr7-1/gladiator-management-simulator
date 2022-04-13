cf70370b670df181fa0f77ae79e480181092b640
ipieropan <igorpieropan@gmail.com>
2022-04-13 21:06:43 +0200

    wp

--- Gladiator.Presentation.Ui/src/pages/battle/BattlePage.js
+++ Gladiator.Presentation.Ui/src/pages/battle/BattlePage.js
@@ -1,12 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import useFetchCallback from "../../hooks/useFetchCallback";
 import LoadingSpinner from "../../Components/LoadingSpinner";
 
-const BattlePage = (props) => {
-    const { gladiatorId, setGladiatorId } = useState();
+const BattlePage = () => {
+    //const { gladiatorId, setGladiatorId } = useState();
 
-    const { isLoading, data: gladiators, fetchError, fetchApi } = useFetchCallback(
-        `/api/battle/roster/player/${props.playerId}`,
+    const {
+        isLoading,
+        data: gladiators,
+        fetchError,
+        fetchApi,
+    } = useFetchCallback(
+        `/api/battle/roster/player/${1}`,
         "GET",
         { "Content-Type": "application/json" },
         null,
@@ -15,7 +20,7 @@ const BattlePage = (props) => {
 
     useEffect(() => {
         fetchApi();
-    }, [props.playerId]);
+    }, []);
 
     if (isLoading || fetchError) {
         return <LoadingSpinner>({fetchError})</LoadingSpinner>;
@@ -26,26 +31,51 @@ const BattlePage = (props) => {
                 <div className="accordion" id="gladiatorsOpenPanel">
                     {gladiators &&
                         gladiators.map((gladiator) => (
-                            <div key={gladiator.gladiator.id} className="accordion-item">
-                                <h2 className="accordion-header" id="panelsStayOpen-headingOne">
-                                    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                                        
+                            <div
+                                key={gladiator.gladiator.id}
+                                className="accordion-item"
+                            >
+                                <h2
+                                    className="accordion-header"
+                                    id="headingOne"
+                                >
+                                    <button
+                                        className="accordion-button"
+                                        type="button"
+                                        data-bs-toggle="collapse"
+                                        data-bs-target="#collapseOne"
+                                        aria-expanded="true"
+                                        aria-controls="collapseOne"
+                                    >
+                                        {gladiator.gladiator.name}
                                     </button>
                                 </h2>
-                                <div id="panelsStayOpen-collapseOne" className="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                                <div
+                                    div
+                                    id="collapseOne"
+                                    className="accordion-collapse collapse show"
+                                    aria-labelledby="headingOne"
+                                    data-bs-parent="#accordionExample"
+                                >
                                     <div className="accordion-body">
                                         <ul>
-                                            <li>Health: {gladiator.gladiator.health}</li>
-                                            <li>Strength: {gladiator.gladiator.strength}</li>
+                                            <li>
+                                                Health:{" "}
+                                                {gladiator.gladiator.health}
+                                            </li>
+                                            <li>
+                                                Strength:{" "}
+                                                {gladiator.gladiator.strength}
+                                            </li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
-                    ))}
+                        ))}
                 </div>
             </div>
             <div className="col">Opponent</div>
         </div>
     );
-}
+};
 export default BattlePage;
