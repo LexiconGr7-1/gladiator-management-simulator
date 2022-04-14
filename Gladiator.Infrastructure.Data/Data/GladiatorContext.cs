2a46240bca2b0e48e38e9f91a114e9dff1b6ffc5
JaniTuoriniemi <janituoriniemi@hotmail.com>
2022-04-14 09:07:56 +0200

    69-progress

--- Gladiator.Infrastructure.Data/Data/GladiatorContext.cs
+++ Gladiator.Infrastructure.Data/Data/GladiatorContext.cs
@@ -1,5 +1,11 @@
 ﻿using Gladiator.Core.Entities;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Gladiator.Infrastructure.Data.Data
 {
@@ -11,5 +17,17 @@ namespace Gladiator.Infrastructure.Data.Data
         public DbSet<School> Schools { get; set; }
         public DbSet<Player> Players { get; set; }
         public DbSet<Arena> Arenas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Core.Entities.Gladiator>().ToTable("Gladiator");
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<School>().ToTable("Schools");
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Player>().ToTable("Gladiator");
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Arena>().ToTable("Arenas");
+        }
     }
 }
--- Gladiator.Presentation.Api/Program.cs
+++ Gladiator.Presentation.Api/Program.cs
@@ -1,8 +1,41 @@
+using Gladiator.Infrastructure.Data.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<GladiatorContext>(options => options.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
+
 
 var app = builder.Build();
 
